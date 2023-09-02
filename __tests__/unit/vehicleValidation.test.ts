afa94163f3f6a014fee5a722089c850ea025aab1
Montazu <59025037+Montazu@users.noreply.github.com>
2023-09-02 23:21:06 +0200

    Add API to add new vehicle
    * Add vehicle validation and unit tests
    
    * Add api error handling and data processor

---
+++ __tests__/unit/vehicleValidation.test.ts
@@ -0,0 +1,30 @@
+import { VehicleValidation } from '@/utils'
+
+describe('VehicleValidation', () => {
+	it('should validate license plate correctly', () => {
+		expect(VehicleValidation.licensePlate('ST7039T')).toBe(true)
+		expect(VehicleValidation.licensePlate('st7039T')).toBe(false)
+		expect(VehicleValidation.licensePlate(' st7039T  ')).toBe(false)
+		expect(VehicleValidation.licensePlate('ABC123#')).toBe(false)
+		expect(VehicleValidation.licensePlate('AB')).toBe(false)
+		expect(VehicleValidation.licensePlate('')).toBe(false)
+	})
+
+	it('should validate VIN correctly', () => {
+		expect(VehicleValidation.vin('VF1RFB00968678630')).toBe(true)
+		expect(VehicleValidation.vin('W0LGB8EL7F1026641')).toBe(true)
+		expect(VehicleValidation.vin('1HGCM82633A12345')).toBe(false)
+		expect(VehicleValidation.vin('12345678901234567')).toBe(false)
+		expect(VehicleValidation.vin('')).toBe(false)
+	})
+
+	it('should validate date correctly', () => {
+		expect(VehicleValidation.date('2023-08-01T00:00:00Z')).toBe(true)
+		expect(VehicleValidation.date('2025-09-01T14:30:00Z')).toBe(false)
+		expect(VehicleValidation.date('2022-07-04')).toBe(true)
+		expect(VehicleValidation.date('-2022-07-04')).toBe(false)
+		expect(VehicleValidation.date('2022-17-04')).toBe(false)
+		expect(VehicleValidation.date('20.12.2500')).toBe(false)
+		expect(VehicleValidation.date('')).toBe(false)
+	})
+})
---
+++ src/app/api/vehicles/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from 'next/server'
+import { DataProcessor, VehicleValidation } from '@/utils'
+
+export const GET = async () => {
+	const vehicles = [
+		{
+			id: 1,
+			name: 'BMW',
+			license_plate: 'KK21881',
+		},
+	]
+
+	return NextResponse.json({ vehicles })
+}
+
+export const POST = async (request: Request) => {
+	const emptyBody = { license_plate: null, vin: null, first_registration_date: null }
+	const body = await request.json().catch(() => emptyBody)
+	const { license_plate, vin, first_registration_date } = body
+
+	if (!license_plate || !vin || !first_registration_date) {
+		const details = []
+		const fieldMessage = 'Pole wymagane'
+		!license_plate && details.push({ field: 'license_plate', message: fieldMessage })
+		!vin && details.push({ field: 'vin', message: fieldMessage })
+		!first_registration_date && details.push({ field: 'first_registration_date', message: fieldMessage })
+		const error = { message: 'Żądanie zawiera niekompletne dane', details }
+		return NextResponse.json({ error }, { status: 400 })
+	}
+
+	const isLicensePlateValid = VehicleValidation.licensePlate(license_plate)
+	const isVinValid = VehicleValidation.vin(vin)
+	const isDateValid = VehicleValidation.date(first_registration_date)
+
+	if (!isLicensePlateValid || !isVinValid || !isDateValid) {
+		const details = []
+		!isLicensePlateValid && details.push({ field: 'license_plate', message: 'Nieprawidłowa tablica rejestracyjna' })
+		!isVinValid && details.push({ field: 'vin', message: 'Nieprawidłowy numer VIN' })
+		!isDateValid && details.push({ field: 'first_registration_date', message: 'Nieprawidłowa data rejestracji' })
+		const error = { message: 'Żądanie zawiera nieprawidłowe dane', details }
+		return NextResponse.json({ error }, { status: 400 })
+	}
+
+	const processedLicensePlate = DataProcessor.removeSpacesAndConvertToUpperCase(license_plate)
+	const processedVin = DataProcessor.removeSpacesAndConvertToUpperCase(vin)
+	const processedFirstRegistrationDate = DataProcessor.parseIsoDate(first_registration_date)
+
+	return NextResponse.json({ id: 1, processedLicensePlate, processedVin, processedFirstRegistrationDate })
+}
