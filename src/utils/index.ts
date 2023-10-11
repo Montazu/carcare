a48c560b8764ad640cb9ef6e8a044cf593dfad92
Montazu <montazu@montazu.pl>
2023-10-11 09:11:07 +0200

    Start Svelte

--- src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { DataProcessor } from './dataProcessor'
-export { VehicleValidation } from './vehicleValidation'
-export { TypeChecker } from './typeChecker'
-export { AuthValidator } from './authValidator'
